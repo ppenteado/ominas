7dc7453c9e57066d69edf7072f3f19974b27cb46
Paulo Penteado <pp.penteado@gmail.com>
2016-12-02 15:20:55 -0800

    Changes to be committed: 	modified:   download_CASSINI.sh changed timestamps location 	modified:   pp_wgetcl.sav error handling

--- download_CASSINI.sh
+++ download_CASSINI.sh
@@ -15,20 +15,20 @@ baseurl="http://naif.jpl.nasa.gov/pub/naif/CASSINI/kernels/"
 dirs=( fk ik lsk pck sclk )
 
 #location for timestamps files
-mkdir -p ~/.ominas/timestamps/cas
+mkdir -p ~/.ominas/timestamps/CASSINI
 
 for dir in "${dirs[@]}"
 do
- ./pp_wget "${baseurl}${dir}/ --localdir=$1/${dir}/ --absolute --timestamps=~/.ominas/timestamps/cas/${dir}.json $@"
+ ./pp_wget "${baseurl}${dir}/ --localdir=$1/${dir}/ --absolute --timestamps=~/.ominas/timestamps/CASSINI/${dir}.json $@"
 done
 
 #special treatment directories (spk and ck, which are large)
 echo "Downloading spks"
-./pp_wget "${baseurl}spk/ --localdir=$1/spk/ $@ --absolute --timestamps=~/.ominas/timestamps/cas/spk.json" # --xpattern=(\.bsp$)|(\.bsp\.lbl$)"
+./pp_wget "${baseurl}spk/ --localdir=$1/spk/ $@ --absolute --timestamps=~/.ominas/timestamps/CASSINI/spk.json" # --xpattern=(\.bsp$)|(\.bsp\.lbl$)"
 
 echo "Downloading cks"
-#./pp_wget "${baseurl}ck/ --localdir=$1/ck/ $@ --absolute --timestamps=~/.ominas/timestamps/cas/ck.json --xpattern="\
+#./pp_wget "${baseurl}ck/ --localdir=$1/ck/ $@ --absolute --timestamps=~/.ominas/timestamps/CASSINI/ck.json --xpattern="\
 #"(\.tar\.gz$)|([[:digit:]]{5}_[[:digit:]]{5}[[:alnum:]]{2}(_(S|C)[[:digit:]]{2})?\.((pdf)|(txt))$)|(_bc_err\.txt$)" #--xpattern=(\.bc$)|(bc\.lbl$)|"
 
-./pp_wget "${baseurl}ck/ --localdir=$1/ck/ $@ --absolute --timestamps=~/.ominas/timestamps/cas/ck.json --pattern="\
+./pp_wget "${baseurl}ck/ --localdir=$1/ck/ $@ --absolute --timestamps=~/.ominas/timestamps/CASSINI/ck.json --pattern="\
 "([[:digit:]]{5}_[[:digit:]]{5}(r[[:alnum:]])|([[:alnum:]]{2}_ISS))(\.bc$)|(bc\.lbl$)"
