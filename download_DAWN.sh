37849e8ab65fa938bfc6bd30d2de9a640e63b129
Paulo Penteado <pp.penteado@gmail.com>
2017-01-09 13:33:39 -0800

    Changes to be committed: 	modified:   configure.sh added generic kernels downloader 	modified:   download_DAWN.sh fixed subsetting to orbital mission 	new file:   download_GENERIC.sh

--- download_DAWN.sh
+++ download_DAWN.sh
@@ -27,10 +27,14 @@ done
 
 #special treatment directories (spk and ck, which are large)
 echo "Downloading spks"
-./pp_wget "${baseurl}spk/ --localdir=$1/spk/ $@ --absolute --timestamps=~/.ominas/timestamps/ --pattern=dawn_rec_(1(1|2|5|6|7))[[:digit:]]{4}-[[:digit:]]{6}_[[:digit:]]{6}_.+((\.bsp$)|(bsp\.lbl$))"
+./pp_wget "${baseurl}spk/ --localdir=$1/spk/ $@ --absolute --timestamps=~/.ominas/timestamps/ --pattern="\
+"dawn_rec_(1(1|2|5|6|7))[[:digit:]]{4}-[[:digit:]]{6}_[[:digit:]]{6}_.+((\.bsp$)|(bsp\.lbl$)) --xpattern="\
+"dawn_rec_120913-[[:digit:]]{6}_[[:digit:]]{6}_.+((\.bsp$)|(bsp\.lbl$))"
 
 echo "Downloading cks"
-./pp_wget "${baseurl}ck/ --localdir=$1/ck/ $@ --absolute --timestamps=~/.ominas/timestamps/ --pattern=dawn_sc_[[:digit:]]{6}_[[:digit:]]{6}((\.bc$)|(bc\.lbl$))"
+./pp_wget "${baseurl}ck/ --localdir=$1/ck/ $@ --absolute --timestamps=~/.ominas/timestamps/ --pattern="\
+"dawn_sc_(1(1|2|5|6|7))[[:digit:]]{4}_[[:digit:]]{6}((\.bc$)|(bc\.lbl$)) --xpattern="\
+"dawn_sc_((110(1|2|3|4|5|6))[[:digit:]]{2})|(121[[:digit:]]{3})_[[:digit:]]{6}((\.bc$)|(bc\.lbl$))"
 #./pp_wget "${baseurl}ck/ --localdir=$1/ck/ $@ --absolute --timestamps=~/.ominas/timestamps/ --xpattern="\
 #"(\.tar\.gz$)|([[:digit:]]{5}_[[:digit:]]{5}[[:alnum:]]{2}(_(S|C)[[:digit:]]{2})?\.((pdf)|(txt))$)|(_bc_err\.txt$)" #--xpattern=(\.bc$)|(bc\.lbl$)|"
 
