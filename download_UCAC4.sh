0e16f776424bff22436204debe83ff160693ce66
Paulo Penteado <pp.penteado@gmail.com>
2017-01-23 14:00:07 -0800

    Changes to be committed: UCAC4 downloader 	new file:   download_UCAC4.sh

---
+++ download_UCAC4.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#script to download and prepare for use a copy of the UCAC4 catalog from CDS
+#Usage:
+#./download_UCAC4.sh /directory/to/place/catalog
+
+
+echo "This script wiill download the UCAC4 catalog from CDS (ftp://cdsarc.u-strasbg.fr/pub/cats/more/UCAC4/u4b/) and prepare its files for use. As of January/2017, this adds to 161 MB of download, and 665 MB of disk space after unpacking."
+
+read -rp "Continue? " ans
+case $ans in
+  [Nn]*)
+    exit
+esac
+
+
+
+baseurlb="ftp://cdsarc.u-strasbg.fr/pub/cats/more/UCAC4/u4b/" #binary files
+baseurli="ftp://cdsarc.u-strasbg.fr/pub/cats/more/UCAC4/u4i/" #index and text files
+
+#standard download (full, nonrecursive) directories
+
+#location for timestamps files
+mkdir -p ~/.ominas/timestamps/UCAC4
+
+./pp_wget "${baseurli}/ --localdir=$1/${dir}/ --absolute --timestamps=~/.ominas/timestamps/ $@"
+./pp_wget "${baseurlb}/ --localdir=$1/${dir}/ --absolute --timestamps=~/.ominas/timestamps/ $@"
+
+
