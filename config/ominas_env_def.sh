17beb96cb769ff5ebadb7d63651393f3373eae33
Paulo Penteado <pp.penteado@gmail.com>
2017-06-27 18:55:11 -0700

    modified:   config/ominas_env_def.sh removed echoing of DFLAG

--- config/ominas_env_def.sh
+++ config/ominas_env_def.sh
@@ -47,7 +47,7 @@ export NV_SPICE_KER
 NV_INS_DETECT=$NV_CONFIG/tab/instrument_detectors.tab
 export NV_INS_DETECT
 
-echo "DFLAG=${DFLAG}"
+#echo "DFLAG=${DFLAG}"
 if [ "${DFLAG}" = 'true' ]; then
     #-----------------------------------------------------------------------------------#
     # DEMO configuration: 																#
