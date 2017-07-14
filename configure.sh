1eee6a01e74c22846eba551d4eb12e144c74b919
Paulo Penteado <pp.penteado@gmail.com>
2017-07-14 09:10:24 -0700

    modified:   configure.sh fixed LD_PRELOAD

--- configure.sh
+++ configure.sh
@@ -671,7 +671,7 @@ LDCMD
 fi
 tail -n +2 ${idlbin} | sed -e "s/APPLICATION=\`basename \$0\`/APPLICATION=idl/g" >> ~/.ominas/ominas
 if [ "${idlversion}" \< "linux84" ] && [ "${idlversion}" \> "linux" ]; then
-  ldp="LD_PRELOAD=${OMINAS}/util/downloader/libcurl.so.4"
+  ldp="LD_PRELOAD=${OMINAS_DIR}/util/downloader/libcurl.so.4"
   cat ~/.ominas/ominas | sed -e "s|exec |${ldp} exec |g" > ~/.ominas/ominas_tmp
   mv -f ~/.ominas/ominas_tmp ~/.ominas/ominas
 fi
@@ -694,7 +694,7 @@ LDCMD
 fi
 tail -n +2 ${idlbin} | sed -e "s/APPLICATION=\`basename \$0\`/APPLICATION=idlde/g" >> ~/.ominas/ominasde
 if [ "${idlversion}" \< "linux84" ] && [ "${idlversion}" \> "linux" ]; then
-  ldp="LD_PRELOAD=${OMINAS}/util/downloader/libcurl.so.4"
+  ldp="LD_PRELOAD=${OMINAS_DIR}/util/downloader/libcurl.so.4"
   cat ~/.ominas/ominasde | sed -e "s|exec |${ldp} exec |g" > ~/.ominas/ominasde_tmp
   mv -f ~/.ominas/ominasde_tmp ~/.ominas/ominasde
 fi
