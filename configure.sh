4eab47575c41c63ee51aa9f29193f826275e09f8
Paulo Penteado <pp.penteado@gmail.com>
2017-11-06 11:35:42 -0800

    modified:   configure.sh fixed prompt 	modified:   util/ominas_version.pro fixed err test

--- configure.sh
+++ configure.sh
@@ -753,8 +753,7 @@ if [ -e "/opt/X11/lib/flat_namespace/" ]; then
 LDCMD
 fi
 tail -n +2 ${idlbin} | sed -e "s/APPLICATION=\`basename \$0\`/APPLICATION=idl/g" >> ~/.ominas/ominas
-#cat ~/.ominas/ominas | sed -e "s|exec \$BIN_DIR/\$APPLICATION|exec \$BIN_DIR/\$APPLICATION -IDL_STARTUP \${OMINAS_DIR}/util/printver.pro |g" > ~/.ominas/ominas_tmp
-cat ~/.ominas/ominas | sed -e "s|\"\$@\" \$APP_ARGS|\"\$@\" \$APP_ARGS -IDL_STARTUP \${OMINAS_DIR}/util/printver.pro -IDL_PROMPT 'OMINAS\>' |g" > ~/.ominas/ominas_tmp
+cat ~/.ominas/ominas | sed -e "s|\"\$@\" \$APP_ARGS|\"\$@\" \$APP_ARGS -IDL_STARTUP \${OMINAS_DIR}/util/printver.pro -IDL_PROMPT \"'OMINAS\> '\" |g" > ~/.ominas/ominas_tmp
 mv -f ~/.ominas/ominas_tmp ~/.ominas/ominas
 if [ "${idlversion}" \< "linux84" ] && [ "${idlversion}" \> "linux" ]; then
   ldp="LD_PRELOAD=${OMINAS_DIR}/util/downloader/libcurl.so.4"
@@ -779,8 +778,7 @@ if [ -e "/opt/X11/lib/flat_namespace/" ]; then
 LDCMD
 fi
 tail -n +2 ${idlbin} | sed -e "s/APPLICATION=\`basename \$0\`/APPLICATION=idlde/g" >> ~/.ominas/ominasde
-#cat ~/.ominas/ominasde | sed -e "s|exec \$BIN_DIR/\$APPLICATION|exec \$BIN_DIR/\$APPLICATION -IDL_STARTUP \${OMINAS_DIR}/util/printver.pro |g" > ~/.ominas/ominasde_tmp
-cat ~/.ominas/ominasde | sed -e "s|\"\$@\" \$APP_ARGS|\"\$@\" \$APP_ARGS -IDL_STARTUP \${OMINAS_DIR}/util/printver.pro -IDL_PROMPT 'OMINAS\>' |g" > ~/.ominas/ominasde_tmp
+cat ~/.ominas/ominasde | sed -e "s|\"\$@\" \$APP_ARGS|\"\$@\" \$APP_ARGS -IDL_STARTUP \${OMINAS_DIR}/util/printver.pro -IDL_PROMPT \"'OMINAS\> '\" |g" > ~/.ominas/ominasde_tmp
 mv -f ~/.ominas/ominasde_tmp ~/.ominas/ominasde
 if [ "${idlversion}" \< "linux84" ] && [ "${idlversion}" \> "linux" ]; then
   ldp="LD_PRELOAD=${OMINAS_DIR}/util/downloader/libcurl.so.4"
--- util/ominas_version.pro
+++ util/ominas_version.pro
@@ -4,7 +4,7 @@ compile_opt idl2,logical_predicate
 gitver='cd '+getenv('OMINAS_DIR')+' && git describe --abbrev=4 --dirty --always --tags'
 spawn,gitver,res,err
 
-if err then begin
+if keyword_set(err) then begin
   readme=getenv('OMINAS_DIR')+path_sep()+'README.md'
   re=strarr(file_lines(readme))
   openr,lun,/get_lun,readme
