34a249570f7078c60d7e94efd72ae8c11acb2651
Paulo Penteado <pp.penteado@gmail.com>
2017-12-11 08:09:12 -0800

    modified:   util/downloader/download_MAPS.sh replaced repo with nasa ominas_maps

--- util/downloader/download_MAPS.sh
+++ util/downloader/download_MAPS.sh
@@ -5,7 +5,7 @@
 
 OWNDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 wget=${OWNDIR}/pp_wget
-echo "This script wiill download the OMINAS maps package from Github (https://github.com/ppenteado/ominas_maps). As of August/2017, this adds to 990 MB of download,"
+echo "This script wiill download the OMINAS maps package from Github (https://github.com/nasa/ominas_maps). As of August/2017, this adds to 990 MB of download,"
 
 
 if [ -z ${ominas_auto+x} ] || [ ${ominas_auto} == 0 ] ; then
@@ -16,7 +16,7 @@ if [ -z ${ominas_auto+x} ] || [ ${ominas_auto} == 0 ] ; then
   esac
 fi
 
-baseurl="https://github.com/ppenteado/ominas_maps"
+baseurl="https://github.com/nasa/ominas_maps"
 #standard download (full, nonrecursive) directories
 
 #location for timestamps files
@@ -24,7 +24,7 @@ mkdir -p ~/.ominas/timestamps/
 ts=`eval echo "~/.ominas/timestamps/"`
 
 if [ ! -d ${1} ]; then
-  git clone https://github.com/ppenteado/ominas_maps ${1}
+  git clone https://github.com/nasa/ominas_maps ${1}
 else
   echo "${1} already exists; skipping git clone, since it requires the destination to be empty"
   git -C ${1} pull
